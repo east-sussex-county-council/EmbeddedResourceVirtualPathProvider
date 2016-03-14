0674f3beaada75c86c749a07a1c8b583e21da3b3
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-03-14 10:31:48 +0000

    Publish errors to Exceptionless

--- EmbeddedResourceVirtualPathProvider/Logger.cs
+++ EmbeddedResourceVirtualPathProvider/Logger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Exceptionless;
 
 namespace EmbeddedResourceVirtualPathProvider
 {
@@ -9,7 +10,11 @@ namespace EmbeddedResourceVirtualPathProvider
     {
         static Logger()
         {
-            LogWarning = (message, ex) => { };
+            LogWarning = (message, ex) =>
+            {
+                ex.Data.Add("Message", message);
+                ex.ToExceptionless().Submit();
+            };
         }
 
         public static Action<string, Exception> LogWarning { get; set; }
